1dab673b3cc541660045ab4de77c7702e59070c6
Konstantin Bulygin <konstantin.bulygin@mgs.bet>
2023-12-14 16:43:12 +0300

    added 3 images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,8 +17,13 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(WriteToConsole)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
